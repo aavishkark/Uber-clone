53ea86a049c2ff4bc3cde9cafd7e99aa2c707b99
Avishkar Kamble <aavishkark@gmail.com>
2025-10-07 13:06:12 +0530

    completed driver card and multiple ride screens

--- components/RideLayout.tsx
+++ components/RideLayout.tsx
@@ -1,13 +1,13 @@
-import { icons } from '@/constants';
-import { router } from 'expo-router';
-import React, { useRef } from 'react';
-import { Text, TouchableOpacity, View, Image} from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import Map from './Map';
-// import BottomSheet, {
-//   BottomSheetView,
-//   BottomSheetScrollView,
-// } from '@gorhom/bottom-sheet';
+import BottomSheet, {
+  BottomSheetView,
+} from "@gorhom/bottom-sheet";
+import { router } from "expo-router";
+import React, { useRef } from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+import Map from "@/components/Map";
+import { icons } from "@/constants";
 
 const RideLayout = ({
   title,
@@ -18,34 +18,41 @@ const RideLayout = ({
   snapPoints?: string[];
   children: React.ReactNode;
 }) => {
-
   const bottomSheetRef = useRef<BottomSheet>(null);
+
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-        <View className='flex-1 bg-white'>
-            <View className='flex flex-col h-screen bg-blue-500'>
-                <View className="flex flex-row absolute z-10 top-16 items-center justify-start px-5">
-                    <TouchableOpacity onPress={() => router.back()}>
-                    <View className="w-10 h-10 bg-white rounded-full items-center justify-center">
-                        <Image
-                        source={icons.backArrow}
-                        resizeMode="contain"
-                        className="w-6 h-6"
-                        />
-                    </View>
-                    </TouchableOpacity>
-                    <Text className="text-xl font-JakartaSemiBold ml-5">
-                    {title || "Go Back"}
-                    </Text>
-                </View>
-                <Map/>
-            </View>
-            {/* <BottomSheet
-        ref={bottomSheetRef}
-      >
-        {children}
-      </BottomSheet> */}
+    <GestureHandlerRootView className="flex-1">
+      <View className="flex-1 bg-white">
+        <View className="flex-1 bg-blue-500">
+          <View className="flex flex-row absolute z-10 top-16 items-center justify-start px-5">
+            <TouchableOpacity onPress={() => router.back()}>
+              <View className="w-10 h-10 bg-white rounded-full items-center justify-center">
+                <Image
+                  source={icons.backArrow}
+                  resizeMode="contain"
+                  className="w-6 h-6"
+                />
+              </View>
+            </TouchableOpacity>
+            <Text className="text-xl font-JakartaSemiBold ml-5">
+              {title || "Go Back"}
+            </Text>
+          </View>
+
+          <Map />
+          <BottomSheet
+            keyboardBehavior="extend"
+            enableDynamicSizing={false}
+            ref={bottomSheetRef}
+            snapPoints={snapPoints || ["40%", "85%"]}
+            index={0}
+          >
+            <BottomSheetView style={{flex:1, padding:20}}>
+              {children}
+            </BottomSheetView>
+          </BottomSheet>
         </View>
+      </View>
     </GestureHandlerRootView>
   );
 };
